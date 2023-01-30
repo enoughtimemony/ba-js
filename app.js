161e1d44a9dcee36fef220e0e980ea70fe2459b7
enoughtimemony <121159776+enoughtimemony@users.noreply.github.com>
2023-01-31 03:28:14 +0900

    first

---
+++ app.js
@@ -0,0 +1,18 @@
+const imFat = false;
+let realName = "태혁";
+console.log(realName);
+
+const a = 10;
+const b = 200;
+let crazyMan = "crazy";
+
+console.log(a * b * b);
+console.log(a + b);
+console.log(b - a);
+console.log(a - b);
+
+console.log("take it easy " + crazyMan);
+
+crazyMan = "lazy";
+
+console.log("take it easy " + crazyMan);
