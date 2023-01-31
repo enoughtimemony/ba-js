32dd3afe4a8ffe8eb322c5a48a23a367e5660408
enoughtimemony <121159776+enoughtimemony@users.noreply.github.com>
2023-02-01 04:08:03 +0900

    ㅜㅡㅜ

--- app.js
+++ app.js
@@ -16,3 +16,29 @@ console.log("take it easy " + crazyMan);
 crazyMan = "lazy";
 
 console.log("take it easy " + crazyMan);
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+const nonsense = [1, 2, "hello", true, false, null];
+
+//get item from Array
+console.log(daysOfWeek);
+
+//Add one monre day to the Array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+const player = {
+  name: "chaewon",
+  points: 77777,
+  pretty: true,
+  balance: "perfect",
+};
+
+console.log(player);
+console.log(player.name);
+
+player.dance = "perfect";
+player.points = player.points + 20;
+console.log(player);
