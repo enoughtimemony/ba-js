21712fbed115471742475e2a4d8f366a0a00df90
enoughtimemony <121159776+enoughtimemony@users.noreply.github.com>
2023-04-05 01:22:38 +0900

    lazy

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-const imFat = false;
+//different
 let realName = "태혁";
 console.log(realName);
 
-const a = 10;
-const b = 200;
 let crazyMan = "crazy";
-
-console.log(a * b * b);
-console.log(a + b);
-console.log(b - a);
-console.log(a - b);
 
 console.log("take it easy " + crazyMan);
 
 crazyMan = "lazy";
 
 console.log("take it easy " + crazyMan);
+//ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
 
 const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
 
-const nonsense = [1, 2, "hello", true, false, null];
+console.log(daysOfWeek[2]);
 
 //get item from Array
 console.log(daysOfWeek);
@@ -42,3 +36,54 @@ console.log(player.name);
 player.dance = "perfect";
 player.points = player.points + 20;
 console.log(player);
+
+// #2.7
+
+function sayHello() {
+  console.log("Hello my name is ");
+}
+
+sayHello();
+sayHello();
+sayHello();
+
+///////////
+
+function sayHelloq(nameOfPerson, age) {
+  console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+}
+sayHelloq("nico", 10);
+sayHelloq("dal", 23);
+sayHelloq("lynn", 21);
+
+//////////////
+
+function plus(fisrstNumber, secondNumer) {
+  console.log(fisrstNumber + secondNumer);
+}
+
+plus(8, 20);
+plus(8, 210);
+plus(89, 20);
+
+const user = {
+  name: "nico",
+  sayHello: function (otherPersonName) {
+    console.log("hello! " + otherPersonName);
+  },
+};
+
+console.log(user.name);
+user.sayHello("lynn");
+
+const cal = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (c, d) {
+    console.log(c - d);
+  },
+};
+
+cal.add(100, 20);
+cal.minus(200, 100);
